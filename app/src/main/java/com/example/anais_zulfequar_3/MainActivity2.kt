7a5a1e1417822d6896209fbbd3c4af1a0850150b
anaiszed447@gmail.com <anaiszed447@gmail.com>
2020-11-19 22:42:35 -0500

    Created 7 province objects. Each object representing one of the canadian provinces.

--- app/src/main/java/com/example/anais_zulfequar_3/MainActivity2.kt
+++ app/src/main/java/com/example/anais_zulfequar_3/MainActivity2.kt
@@ -23,7 +23,20 @@ class MainActivity2 : AppCompatActivity() {
 
     fun getData(): ArrayList<Province> {
         var list = ArrayList<Province>(0)
-        var p1 = Province("Ontario", "Toronto", R.mipmap.ic_launcher)
+        var p1 = Province("Ontario", "Toronto", R.mipmap.ontario)
+        list.add(p1)
+        var p2 = Province("Quebec", "Quebec", R.mipmap.quebec)
+        list.add(p2)
+        var p3 = Province("Nova Scotia", "Halifax", R.mipmap.nova_scotia)
+        list.add(p3)
+        var p4 = Province("New Brunswick", "Fredericton", R.mipmap.new_brunswick)
+        list.add(p4)
+        var p5 = Province("Manitoba", "Winnipeg", R.mipmap.manitoba)
+        list.add(p5)
+        var p6 = Province("British Columbia", "Victoria", R.mipmap.british_columbia)
+        list.add(p6)
+        var p7 = Province("Prince Edward Island", "Charlottetown", R.mipmap.prince)
+        list.add(p7)
         return list
     }
 
